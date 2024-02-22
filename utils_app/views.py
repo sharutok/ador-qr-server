b29c49be02ce7e0878ca01ed9b1f3813642bb761
Sharan Kudtarkar <SK19@ador.co.in>
2024-02-22 15:19:49 +0530

    02/22/24 15:19:43

--- utils_app/views.py
+++ utils_app/views.py
@@ -193,7 +193,8 @@ def convert_to_pdf(image_path):
 
 def generate_qr(id):
     qr = qrcode.QRCode(version=3, box_size=10, border=1, error_correction=qrcode.constants.ERROR_CORRECT_H)
-    data = "http://localhost3000/share/{}".format(id)
+    link=os.getenv("URL_LINK")
+    data = "{}/share/{}".format(link,id)
     qr.add_data(data)
     qr.make(fit=True)
     img = qr.make_image(fill_color="black", back_color="white")
